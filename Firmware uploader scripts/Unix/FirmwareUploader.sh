a84d2b60ed88c09e4a82849e7a1a0e295d7954b8
kitusmark <electronicslab@icloud.com>
2015-10-16 10:41:40 +0000

    More code

--- Firmware uploader scripts/Unix/FirmwareUploader.sh
+++ Firmware uploader scripts/Unix/FirmwareUploader.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 
 echo -------------------------------------------------------------
-echo \n 
+echo -e " \n"
 echo FIRMWARE UPLOADER FOR BCN3D ELECTRONICS
-echo \n
+echo -e "\n"
+echo ------------------------------------------------------------
+
+echo -e "\n"
+echo Select between uploading the firmware or burning the bootloader.
+echo -e "F" means firmware and "B" means Bootloader.
+echo -e Press "Q" to "exit" the program.
+
+#until command is done when the expression is false
+until [ $command Q]; do
+	echo -e "Please, enter your option: \c "
+	read command
+	if [ $command = "F" ]; then
+		echo F detected, Firmware it is!
+		#Now we're going to load the firmware
+	else if [ $command = "B" ]; then
+		echo B detected, Bootloader it is!
+		#Now we're going to load the Bootloader
+	else
+		echo Please, enter a valid option: F, B or Q to quit
+	fi
+	clear
+done
+
+#If we are here, it means we've read a Q --> quit program
+echo Bye bye!
 
